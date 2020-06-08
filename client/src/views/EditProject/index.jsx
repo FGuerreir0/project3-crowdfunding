f5b230ec7d224a61a7fad6d759217998bc8eb85c
Artur Serra <artur.aserra@gmail.com>
2020-06-08 21:41:06 +0100

    Worked on the edit project view with the constructor

--- client/src/views/EditProject/index.jsx
+++ client/src/views/EditProject/index.jsx
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import axios from 'axios';
 
 class EditProjectView extends Component {
-  componentDidMount() {
-    const { title } = this.props;
-    this.setState({ title });
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      title: '',
+      description: '',
+      location: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  updateInputValue(e) {
-    const {
-      target: { value }
-    } = e;
-    this.setState({ title: value });
+  getProjectDetails() {
+    let projectId = this.props.match.params.id;
+    axios
+      .get(/*Project single page path*/)
+      .then((res) => {
+        this.setState(
+          {
+            id: res.data.id,
+            title: res.data.name,
+            description: res.data.description,
+            location: res.data.location
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      })
+      .catch((err) => console.log(err));
+  }
+
+  editProject(newProject) {
+    // If everything is working, this would handle the form submit
+  }
+
+  onSubmit(e) {
+    const newProject = {
+      title: this.title.value,
+      description: this.description.value,
+      location: this.location.value
+    };
+    this.editProject(newProject);
+    e.preventDefault();
+  }
+
+  handleInputChange(e) {
+    const target = e.target;
+    const value = target.value;
+
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
@@ -25,7 +71,24 @@ class EditProjectView extends Component {
             name='title'
             placeholder='New title'
             value={this.state.title}
-            onChange={this.updateInputValue}
+            onChange={this.handleInputChange}
+          />
+          <input
+            type='text'
+            className='update-project-description'
+            name='description'
+            placeholder='New Description'
+            value={this.state.description}
+            onChange={this.handleInputChange}
+          />
+
+          <input
+            type='text'
+            className='update-project-location'
+            name='title'
+            placeholder='New location'
+            value={this.state.location}
+            onChange={this.handleInputChange}
           />
         </form>
       </div>
