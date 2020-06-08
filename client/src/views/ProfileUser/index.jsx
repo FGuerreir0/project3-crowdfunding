c12fc725fb47ee3aba45acf80d3e5a6889fcfceb
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-06-08 18:32:58 +0100

    Started on profile

--- client/src/views/ProfileUser/index.jsx
+++ client/src/views/ProfileUser/index.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import { Link } from 'react-router-dom';
 
 class ProfileUserView extends Component {
   constructor(props) {
     super(props);
   }
+
   render(props) {
-    console.log(this.props.user);
+    console.log(this.state);
     return (
-      <div>
-        <h1>This is the Profile User Project View</h1>
-        <h2>{this.props.user.username}'s Profile</h2>
+      <div className=''>
+        <img src={this.props.user.pictureUrl} alt='Profile' className='profile_img' />
+        )}
+        <br></br>
+        <h1>{this.props.user.username}</h1>
+        <br></br>
+        <h2>Email: {this.props.user.email}</h2>
+        <h2>Location: {this.props.user.location}</h2>
+        <br></br>
+        <p>My Projects</p>
+        <br></br>
+        <p>Contributed Actions</p>
+        <Link to={`/user/${this.props.user._id}/edit`}>Edit</Link>
       </div>
     );
   }
