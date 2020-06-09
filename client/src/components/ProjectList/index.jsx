1d49caa7ec0a9dc9b3a666aa0b1fdc0b2e493a90
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-06-09 13:10:21 +0100

    Format Date

--- client/src/components/ProjectList/index.jsx
+++ client/src/components/ProjectList/index.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
+import formatDate from './../../helper/formatDate';
 
 const ProjectsList = (props) => {
   return (
     <div>
       {props.projects.map((project) => (
-        <Link to={`/project/${project._id}`} key={project._id}>
+        <div key={project._id}>
           <div>
-            <div>
-              <img src={project.coverPictureUrl} alt={project.title} />
-              <div>
-                <small>Created by: {project.creator.username}</small>
-                <small>Date:{project.updatedDate}</small>
-              </div>
-            </div>
-            <div>
-              <h2>{project.title}</h2>
-              <p>{project.shortDescription}</p>
+            <img src={project.coverPictureUrl} alt={project.title} />
+            <div className='project_dateCreator'>
+              <small>Created by: {project.creator.username}</small>
+              <small>Date:{formatDate(project.updatedDate)}</small>
             </div>
           </div>
-        </Link>
+          <div className='project_information'>
+            <h2>{project.title}</h2>
+            <p>{project.shortDescription}</p>
+            <p>Show ProgressBar</p>
+          </div>
+          <div className='anchor_style'>
+            <Link to={`/project/${project._id}`}>Read more here</Link>
+          </div>
+        </div>
       ))}
     </div>
   );
