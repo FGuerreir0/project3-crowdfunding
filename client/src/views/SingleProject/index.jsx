3674179d3966dd5bc6729a2e8d2d66a72c4b426d
Artur Serra <artur.aserra@gmail.com>
2020-06-09 10:26:04 +0100

    Project single view

--- client/src/views/SingleProject/index.jsx
+++ client/src/views/SingleProject/index.jsx
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
 import './styles.scss';
 
+import { getProjectById } from './../../services/project';
+
 class SingleProjectView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      project: null
+    };
+  }
+
+  getSingleProject() {
+    const id = this.props.match.params.id;
+    getProjectById(id)
+      .then((project) => {
+        this.setState({
+          loaded: true,
+          project
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.getSingleProject();
+  }
+
   render() {
+    const project = this.state.project;
+
     return (
       <div>
-        <h1>This is the Single Project View</h1>
+        {this.state.loaded && (
+          <>
+            <span>Loading... </span>
+          </>
+        )}
+
+        {project && (
+          <>
+            <img src={project.coverPictureUrl} alt={project.title} />
+            <h1>{project.title}</h1>
+            <small>{/* TODO: Contribution - (Backed/Total) */}</small>
+            <div>
+              <h2>{project.location}</h2>
+              <p>{project.description}</p>
+            </div>
+          </>
+        )}
       </div>
     );
   }
