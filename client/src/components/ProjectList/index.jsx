f0180261d4a5fbe6e852a773a7ea66995ccc0353
Cesar Mello <cesarpcj@hotmail.com>
2020-06-12 11:34:33 +0100

    fixed findout button

--- client/src/components/ProjectList/index.jsx
+++ client/src/components/ProjectList/index.jsx
@@ -42,9 +42,12 @@ const ProjectsList = (props) => {
                     ''}
                 </div>
                 <div className='px-6 py-4'>#{project.category}</div>
-                <div className='bg-blue-500 hover:bg-blue-700 text-center text-white font-bold py-2 px-4 border border-blue-700 rounded'>
-                  {props.user && <Link to={`/project/${project._id}`}>Find out more</Link>}
-                </div>
+
+                {props.user && (
+                  <div className='bg-blue-500 hover:bg-blue-700 text-center text-white font-bold py-2 px-4 border border-blue-700 rounded'>
+                    <Link to={`/project/${project._id}`}>Find out more</Link>
+                  </div>
+                )}
               </div>
             ))}
           </div>
